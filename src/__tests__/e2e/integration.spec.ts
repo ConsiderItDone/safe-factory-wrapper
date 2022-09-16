c413d442082b1f00f8f3d227bd6cbc67058169b8
Mike <mikelrap@gmail.com>
2022-09-16 11:29:54 +0300

    getchainid

--- src/__tests__/e2e/integration.spec.ts
+++ src/__tests__/e2e/integration.spec.ts
@@ -20,7 +20,7 @@ export const SAFE_BASE_VERSION = "1.1.1";
 const txOverrides = { gasLimit: "1000000", gasPrice: "20" };
 const owners = ["0xd405aebF7b60eD2cb2Ac4497Bddd292DEe534E82"];
 
-describe("deploySafe", () => {
+describe("SafeFactory", () => {
   const CONNECTION = { networkNameOrChainId: "ropsten" };
 
   let client: PolywrapClient;
@@ -49,179 +49,197 @@ describe("deploySafe", () => {
     await stopTestEnvironment();
   });
 
-  it("should fail if there are no owners", async () => {
-    const deploySafeResponse = await App.Factory_Module.deploySafe(
-      {
-        safeAccountConfig: {
-          owners: [],
-          threshold: 1,
+  describe("getChainId", () => {
+    it("should return the chainId of the current network", async () => {
+      const chainIdResponse = await App.Factory_Module.getChainId(
+        {
+          connection: CONNECTION,
         },
-        connection: CONNECTION,
-        txOverrides: txOverrides,
-      },
-      client,
-      wrapperUri
-    );
+        client,
+        wrapperUri
+      );
 
-    expect(deploySafeResponse.error).toBeTruthy();
-    expect(deploySafeResponse.data).toBeFalsy();
+      expect(chainIdResponse).toBeTruthy();
+      expect(chainIdResponse.data).toBeTruthy();
+      expect(chainIdResponse.error).toBeFalsy();
+    });
   });
 
-  it("should fail if the threshold is lower than 0", async () => {
-    const deploySafeResponse = await App.Factory_Module.deploySafe(
-      {
-        safeAccountConfig: {
-          owners: owners,
-          threshold: -1,
+  describe("deploySafe", () => {
+    it("should fail if there are no owners", async () => {
+      const deploySafeResponse = await App.Factory_Module.deploySafe(
+        {
+          safeAccountConfig: {
+            owners: [],
+            threshold: 1,
+          },
+          connection: CONNECTION,
+          txOverrides: txOverrides,
         },
-        connection: CONNECTION,
-        txOverrides: txOverrides,
-      },
-      client,
-      wrapperUri
-    );
+        client,
+        wrapperUri
+      );
 
-    expect(deploySafeResponse.error).toBeTruthy();
-    expect(deploySafeResponse.data).toBeFalsy();
-  });
+      expect(deploySafeResponse.error).toBeTruthy();
+      expect(deploySafeResponse.data).toBeFalsy();
+    });
 
-  it("should fail if the threshold is higher than the owners length", async () => {
-    const deploySafeResponse = await App.Factory_Module.deploySafe(
-      {
-        safeAccountConfig: {
-          owners: owners,
-          threshold: 2,
+    it("should fail if the threshold is lower than 0", async () => {
+      const deploySafeResponse = await App.Factory_Module.deploySafe(
+        {
+          safeAccountConfig: {
+            owners: owners,
+            threshold: -1,
+          },
+          connection: CONNECTION,
+          txOverrides: txOverrides,
         },
-        connection: CONNECTION,
-        txOverrides: txOverrides,
-      },
-      client,
-      wrapperUri
-    );
+        client,
+        wrapperUri
+      );
 
-    expect(deploySafeResponse.error).toBeTruthy();
-    expect(deploySafeResponse.data).toBeFalsy();
-  });
+      expect(deploySafeResponse.error).toBeTruthy();
+      expect(deploySafeResponse.data).toBeFalsy();
+    });
 
-  it("should fail if the saltNonce is lower than 0", async () => {
-    const deploySafeResponse = await App.Factory_Module.deploySafe(
-      {
-        safeAccountConfig: {
-          owners: owners,
-          threshold: 2,
+    it("should fail if the threshold is higher than the owners length", async () => {
+      const deploySafeResponse = await App.Factory_Module.deploySafe(
+        {
+          safeAccountConfig: {
+            owners: owners,
+            threshold: 2,
+          },
+          connection: CONNECTION,
+          txOverrides: txOverrides,
         },
-        safeDeploymentConfig: {
-          saltNonce: "-2",
+        client,
+        wrapperUri
+      );
+
+      expect(deploySafeResponse.error).toBeTruthy();
+      expect(deploySafeResponse.data).toBeFalsy();
+    });
+
+    it("should fail if the saltNonce is lower than 0", async () => {
+      const deploySafeResponse = await App.Factory_Module.deploySafe(
+        {
+          safeAccountConfig: {
+            owners: owners,
+            threshold: 2,
+          },
+          safeDeploymentConfig: {
+            saltNonce: "-2",
+          },
+          connection: CONNECTION,
+          txOverrides: txOverrides,
         },
-        connection: CONNECTION,
-        txOverrides: txOverrides,
-      },
-      client,
-      wrapperUri
-    );
+        client,
+        wrapperUri
+      );
 
-    expect(deploySafeResponse.error).toBeTruthy();
-    expect(deploySafeResponse.data).toBeFalsy();
-  });
+      expect(deploySafeResponse.error).toBeTruthy();
+      expect(deploySafeResponse.data).toBeFalsy();
+    });
 
-  it("should deploy a new Safe without saltNonce", async () => {
-    const deploySafeResponse = await App.Factory_Module.deploySafe(
-      {
-        safeAccountConfig: {
-          owners: owners,
-          threshold: 1,
+    it("should deploy a new Safe without saltNonce", async () => {
+      const deploySafeResponse = await App.Factory_Module.deploySafe(
+        {
+          safeAccountConfig: {
+            owners: owners,
+            threshold: 1,
+          },
+          connection: CONNECTION,
+          txOverrides: txOverrides,
         },
-        connection: CONNECTION,
-        txOverrides: txOverrides,
-      },
-      client,
-      wrapperUri
-    );
+        client,
+        wrapperUri
+      );
 
-    expect(deploySafeResponse.error).toBeFalsy();
-    expect(deploySafeResponse.data).toBeTruthy();
-  });
+      expect(deploySafeResponse.error).toBeFalsy();
+      expect(deploySafeResponse.data).toBeTruthy();
+    });
 
-  it("should deploy a new Safe with saltNonce", async () => {
-    const deploySafeResponse = await App.Factory_Module.deploySafe(
-      {
-        safeAccountConfig: {
-          owners: owners,
-          threshold: 1,
+    it("should deploy a new Safe with saltNonce", async () => {
+      const deploySafeResponse = await App.Factory_Module.deploySafe(
+        {
+          safeAccountConfig: {
+            owners: owners,
+            threshold: 1,
+          },
+          safeDeploymentConfig: {
+            saltNonce: Date.now().toString(),
+          },
+          connection: CONNECTION,
+          txOverrides: txOverrides,
         },
-        safeDeploymentConfig: {
-          saltNonce: Date.now().toString(),
+        client,
+        wrapperUri
+      );
+
+      expect(deploySafeResponse.error).toBeFalsy();
+      expect(deploySafeResponse.data).toBeTruthy();
+    });
+
+    it("should deploy last Safe version by default", async () => {
+      const deploySafeResponse = await App.Factory_Module.deploySafe(
+        {
+          safeAccountConfig: {
+            owners: owners,
+            threshold: 1,
+          },
+          connection: CONNECTION,
+          txOverrides: txOverrides,
         },
-        connection: CONNECTION,
-        txOverrides: txOverrides,
-      },
-      client,
-      wrapperUri
-    );
+        client,
+        wrapperUri
+      );
 
-    expect(deploySafeResponse.error).toBeFalsy();
-    expect(deploySafeResponse.data).toBeTruthy();
-  });
+      expect(deploySafeResponse.error).toBeTruthy();
+      expect(deploySafeResponse.data).toBeFalsy();
+    });
 
-
-  it("should deploy last Safe version by default", async () => {
-    const deploySafeResponse = await App.Factory_Module.deploySafe(
-      {
-        safeAccountConfig: {
-          owners: owners,
-          threshold: 1,
+    it("should fail a specific Safe version on unsupported chain", async () => {
+      const deploySafeResponse = await App.Factory_Module.deploySafe(
+        {
+          safeAccountConfig: {
+            owners: owners,
+            threshold: 1,
+          },
+          safeDeploymentConfig: {
+            saltNonce: Date.now().toString(),
+            version: "1.2.0",
+          },
+          connection: CONNECTION,
+          txOverrides: txOverrides,
         },
-        connection: CONNECTION,
-        txOverrides: txOverrides,
-      },
-      client,
-      wrapperUri
-    );
+        client,
+        wrapperUri
+      );
 
-    expect(deploySafeResponse.error).toBeTruthy();
-    expect(deploySafeResponse.data).toBeFalsy();
-  });
-  it("should fail a specific Safe version on unsupported chain", async () => {
-    const deploySafeResponse = await App.Factory_Module.deploySafe(
-      {
-        safeAccountConfig: {
-          owners: owners,
-          threshold: 1,
-        },
-        safeDeploymentConfig: {
-          saltNonce: Date.now().toString(),
-          version: "1.2.0",
-        },
-        connection: CONNECTION,
-        txOverrides: txOverrides,
-      },
-      client,
-      wrapperUri
-    );
+      expect(deploySafeResponse.error).toBeTruthy();
+      expect(deploySafeResponse.data).toBeFalsy();
+    });
 
-    expect(deploySafeResponse.error).toBeTruthy();
-    expect(deploySafeResponse.data).toBeFalsy();
-  });
-
-  it("should deploy a specific Safe version", async () => {
-    const deploySafeResponse = await App.Factory_Module.deploySafe(
-      {
-        safeAccountConfig: {
-          owners: owners,
-          threshold: 1,
+    it("should deploy a specific Safe version", async () => {
+      const deploySafeResponse = await App.Factory_Module.deploySafe(
+        {
+          safeAccountConfig: {
+            owners: owners,
+            threshold: 1,
+          },
+          safeDeploymentConfig: {
+            saltNonce: Date.now().toString(),
+            version: "1.3.0",
+          },
+          connection: CONNECTION,
+          txOverrides: txOverrides,
         },
-        safeDeploymentConfig: {
-          saltNonce: Date.now().toString(),
-          version: "1.3.0",
-        },
-        connection: CONNECTION,
-        txOverrides: txOverrides,
-      },
-      client,
-      wrapperUri
-    );
+        client,
+        wrapperUri
+      );
 
-    expect(deploySafeResponse.error).toBeFalsy();
-    expect(deploySafeResponse.data).toBeTruthy();
+      expect(deploySafeResponse.error).toBeFalsy();
+      expect(deploySafeResponse.data).toBeTruthy();
+    });
   });
 });
