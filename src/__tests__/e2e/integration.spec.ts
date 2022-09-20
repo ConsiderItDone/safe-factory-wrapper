7c1521946667f762df77af1feaa26d52366db5f9
n0cte <ilnur.a.g@gmail.com>
2022-09-20 10:21:32 +0300

    update nonce

--- src/__tests__/e2e/integration.spec.ts
+++ src/__tests__/e2e/integration.spec.ts
@@ -311,7 +311,7 @@ describe("SafeFactory", () => {
     });
 
     it('should predict a new Safe with saltNonce', async () =>  {
-      const saltNonce = "12345";
+      const saltNonce = "0x125";
       const predictSafeResp = await App.Factory_Module.predictSafeAddress(
         {
           safeAccountConfig: {
